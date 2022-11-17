a64a0f775a854b3aec1dafa17c54bcd406bee1f0
breroeluean <60568024+breroeluean@users.noreply.github.com>
2022-11-17 23:54:23 +0100

    workers

--- Allocator/Main.java
+++ Allocator/Main.java
@@ -1,19 +1,51 @@
 package Allocator;
 
+import java.util.*;
+
+class Worker extends Thread {
+    private static int count = 0;
+
+    private Allocator allocator;
+
+    private Random random;
+
+    private int id;
+    
+    public Worker() {
+        allocator = AllocatorImplementation.getInstance();
+        random = new Random();
+        id = count++;
+    }
+
+    public void run() {
+        int amount = random.nextInt(8,10000);
+
+        Long address = allocator.allocate(amount);
+        System.out.println("Allocated " + amount + " bytes at " + address + "[" + id + "]");
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {}
+
+        System.out.println(allocator.isAccessible(address) + "[" + id + "]");
+        
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {}
+
+        allocator.free(address);
+        System.out.println("Freed " + amount + " bytes at " + address + "[" + id + "]");
+    }
+}
+
 public class Main {
  
     public static void main(String[] args) {
-        Allocator allocator = AllocatorImplementation.getInstance();
-
-        int amount = 8000;
-
-        Long address = allocator.allocate(amount);
-        System.out.println("Allocated " + amount + " bytes at " + address);
-
-        System.out.println(allocator.isAccessible(address));
         
-        allocator.free(address);
-        System.out.println("Freed " + amount + " bytes at " + address);
+        for(int i = 0; i < 5; i++) {
+            new Worker().start();
+        }
+        
     }
 
 }
